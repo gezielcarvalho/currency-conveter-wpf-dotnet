83500d28accd6a52a6b8e755bb605762d5fd7d99
Geziel Carvalho <geziel.natal@gmail.com>
2024-07-12 15:52:37 -0400

    270. Showing Data in a ListBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -23,41 +24,48 @@ namespace currency_converter_wpf_dotnet
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConection;
         public MainWindow()
         {
             InitializeComponent();
-            BindCurrency();
-
             string connectionString = ConfigurationManager.ConnectionStrings["currency_converter_wpf_dotnet.Properties.Settings.CurrencyConverterDbConnectionString"].ConnectionString;
+            sqlConection = new SqlConnection(connectionString);
+            BindCurrency();
         }
 
         private void BindCurrency()
         {
-            // Bind the currency to the label
-            DataTable dtcurrency = new DataTable();
-            dtcurrency.Columns.Add("Text");
-            dtcurrency.Columns.Add("Value");
+            try
+            {
+                // Open the connection
+                sqlConection.Open();
+                string query = "SELECT * FROM Currency";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConection);
 
-            dtcurrency.Rows.Add("Select Currency", "0");
-            dtcurrency.Rows.Add("USD", 1);
-            dtcurrency.Rows.Add("EUR", 0.85);
-            dtcurrency.Rows.Add("GBP", 0.75);
-            dtcurrency.Rows.Add("INR", 73.85);
-            dtcurrency.Rows.Add("AUD", 1.35);
-            dtcurrency.Rows.Add("CAD", 1.32);
-            dtcurrency.Rows.Add("SGD", 1.43);
-            dtcurrency.Rows.Add("CHF", 0.91);
-            dtcurrency.Rows.Add("MYR", 4.18);
+                // Bind the currency to the label
+                DataTable dtcurrency = new DataTable();
+                dtcurrency.Columns.Add("CurrencyCode");
+                dtcurrency.Columns.Add("CurrencyId");
 
-            cmbFromCurrency.ItemsSource = dtcurrency.DefaultView;
-            cmbFromCurrency.DisplayMemberPath = "Text";
-            cmbFromCurrency.SelectedValuePath = "Value";
-            cmbFromCurrency.SelectedIndex = 0;
+                dtcurrency.Rows.Add("Select Currency", "0");
 
-            cmbToCurrency.ItemsSource = dtcurrency.DefaultView;
-            cmbToCurrency.DisplayMemberPath = "Text";
-            cmbToCurrency.SelectedValuePath = "Value";
-            cmbToCurrency.SelectedIndex = 0;
+                adapter.Fill(dtcurrency);
+                sqlConection.Close();
+
+                cmbFromCurrency.ItemsSource = dtcurrency.DefaultView;
+                cmbFromCurrency.DisplayMemberPath = "CurrencyCode";
+                cmbFromCurrency.SelectedValuePath = "CurrencyId";
+                cmbFromCurrency.SelectedIndex = 0;
+
+                cmbToCurrency.ItemsSource = dtcurrency.DefaultView;
+                cmbToCurrency.DisplayMemberPath = "CurrencyCode";
+                cmbToCurrency.SelectedValuePath = "CurrencyId";
+                cmbToCurrency.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) //Allow Only Integer in Text Box
         {
